4103871995b29c7ad9c6304539bbb31247769cf3
Oussama BASRY <58572674+oussamabasry@users.noreply.github.com>
2021-05-10 19:16:23 +0000

    first commit

---
+++ routes/UserRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const UserController = require("../controllers/UserController");
+const checkAuth = require("../middlewares/check-auth");
+
+const router = express.Router();
+
+router.post("/signup", UserController.signup);
+router.post("/login", UserController.login);
+router.get("/:userId", checkAuth, UserController.getOneUser);
+
+module.exports = router;
