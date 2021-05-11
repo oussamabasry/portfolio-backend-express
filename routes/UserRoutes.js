398f6a77cd5558cd1c282efb66111d5cc906775c
Oussama BASRY <58572674+oussamabasry@users.noreply.github.com>
2021-05-11 18:35:06 +0000

    auth

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -5,7 +5,7 @@ const checkAuth = require("../middlewares/check-auth");
 
 const router = express.Router();
  
-router.post("/signup", UserController.signup);
+router.post("/signup", checkAuth, UserController.signup);
 router.post("/login", UserController.login);
 router.get("/:userId", checkAuth, UserController.getOneUser);
 
